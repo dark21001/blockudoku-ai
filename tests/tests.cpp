461a4a75b9824fa68f865a7dcde08983cfd624ff
Gary <gary-z@users.noreply.github.com>
2021-11-30 18:51:25 -0800

    Boosted

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,11 +1,12 @@
 #pragma once
 #include <stdlib.h>
 #include "../engine.h"
+#include "boostassert.h"
 
 int main()
 {
 	BitBoard B = BitBoard(0, 0);
-	return 1;
+	BOOST_ASSERT_MSG(1 == 2, "bleh");
+	return 0;
 }
-
 
