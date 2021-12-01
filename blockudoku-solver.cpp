613a2b2eb6e9346348716ebe535cc588b910d609
Gary <gary-z@users.noreply.github.com>
2021-12-01 01:23:43 -0800

    build

--- blockudoku-solver.cpp
+++ blockudoku-solver.cpp
@@ -8,6 +8,17 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello CMake!!\n" << BitBoard::column(2).str() << endl;
+	BitBoard acc_cubes = BitBoard::empty();
+	cout << (BitBoard::row(3) | BitBoard::column(4)).str() << endl;
+	
+	for (int r = 0; r < 3; r++) {
+		for (int c = 0; c < 3; c++) {
+			BitBoard cube = BitBoard::cube(r, c);
+			acc_cubes = acc_cubes | cube;
+			cout << cube.str() << endl;
+		}
+	}
+
+	cout << "Hello CMake!!\n" << acc_cubes.str() << endl;
 	return 0;
 }
