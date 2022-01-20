37f613c9c36b2335f75a689d6c28c939e6d9feaf
Gary <gary-z@users.noreply.github.com>
2022-01-19 18:44:22 -0800

    Split fitness test.

--- blockudoku-ai.cpp
+++ blockudoku-ai.cpp
@@ -2,12 +2,7 @@
 //
 
 #include "engine.h"
-#include <algorithm>
-#include <vector>
 #include <iostream>
-#include <atomic>
-#include <thread>
-#include <numeric>
 
 using namespace std;
 
@@ -20,52 +15,6 @@ void printPieceSet(PieceSet piece_set) {
 		}
 		cout << endl;
 	}
-}
-
-int getNumTurnsSample() {
-	auto game = GameState(BitBoard::empty());
-	int score = 0;
-
-	while(true) {
-		score += 1;
-		const auto piece_set = PieceSet::getRandom();
-		const auto next = AI::makeMoveSimple(game, piece_set);
-		if (next.isOver()){
-			cout << game.getBitBoard().str() << endl;
-			printPieceSet(piece_set);
-			break;
-		}
-		game = next;
-	}
-	return score;
-}
-
-// Call this to test changes to the evaluation function.
-double simpleEvalFitnessTest(int numGames) {
-	atomic<int> games_done(0);
-	vector<double> scores;
-	std::vector<std::thread> workers;
-    for (int i = 0; i < std::thread::hardware_concurrency(); i++) {
-        workers.push_back(std::thread([&]()
-        {
-			while (games_done++ < numGames){
-				const auto score = getNumTurnsSample();
-				scores.push_back(score);
-				cout << scores.size() << '/' << numGames << ' ' << score << endl;
-			}
-        }));
-    }
-	std::for_each(workers.begin(), workers.end(), [](std::thread &t)
-    {
-        t.join();
-    });
-
-	sort(scores.begin(), scores.end());
-	double result = scores[numGames / 2];
-	cout << "p50: " << result << endl;
-	cout << "avg: " << (std::accumulate(scores.begin(), scores.end(), 0.0) / numGames) << endl;
-
-	return result;
 }
 
 int main() {
---
+++ fitness.cpp
@@ -0,0 +1,65 @@
+// blockudoku-solver.cpp : Defines the entry point for the application.
+//
+
+#include "engine.h"
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <atomic>
+#include <thread>
+#include <numeric>
+#include <cstdlib>
+
+using namespace std;
+
+int getNumTurnsSample() {
+	auto game = GameState(BitBoard::empty());
+	int score = 0;
+
+	while(true) {
+		score += 1;
+		const auto piece_set = PieceSet::getRandom();
+		const auto next = AI::makeMoveSimple(game, piece_set);
+		if (next.isOver()){
+			break;
+		}
+		game = next;
+	}
+	return score;
+}
+
+// Call this to test changes to the evaluation function.
+double simpleEvalFitnessTest(int numGames) {
+	atomic<int> games_done(0);
+	vector<double> scores;
+	std::vector<std::thread> workers;
+    for (int i = 0; i < std::thread::hardware_concurrency(); i++) {
+        workers.push_back(std::thread([&]()
+        {
+			while (games_done++ < numGames){
+				const auto score = getNumTurnsSample();
+				scores.push_back(score);
+				cout << scores.size() << '/' << numGames << ' ' << score << endl;
+			}
+        }));
+    }
+	std::for_each(workers.begin(), workers.end(), [](std::thread &t)
+    {
+        t.join();
+    });
+
+	sort(scores.begin(), scores.end());
+	double result = scores[numGames / 2];
+	cout << "p50: " << result << endl;
+	cout << "avg: " << (std::accumulate(scores.begin(), scores.end(), 0.0) / numGames) << endl;
+
+	return result;
+}
+
+int main(int argc, char *argv[]) {
+	srand((unsigned)time(NULL));
+    const int num_iterations = std::atoi(argv[1]);
+    simpleEvalFitnessTest(num_iterations);
+
+	return 0;
+}
